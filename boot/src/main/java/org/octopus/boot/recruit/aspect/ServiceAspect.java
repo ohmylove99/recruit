3ba60eb156c52d7ca3cd4b63c9c4673a93da55cb
ohmylove99 <lengfei9928@hotmail.com>
2017-05-01 09:19:57 +0800

    Add logback

--- boot/src/main/java/org/octopus/boot/recruit/aspect/ServiceAspect.java
+++ boot/src/main/java/org/octopus/boot/recruit/aspect/ServiceAspect.java
@@ -3,15 +3,18 @@ package org.octopus.boot.recruit.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class ServiceAspect {
+	private static final Logger logger = LoggerFactory.getLogger(ServiceAspect.class);
 
 	@AfterReturning("execution(* org.octopus..*Service.*(..))")
 	public void logServiceAccess(JoinPoint joinPoint) {
-		System.out.println("Completed: " + joinPoint);
+		logger.info("Completed: " + joinPoint);
 	}
 
 }
